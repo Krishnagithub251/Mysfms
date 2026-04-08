6825cab9201104086282ebd57dccf20991acfad0
hari <valluriharikrishna2@gmail.com>
2026-04-09 00:47:30 +0530

    hari commit

---
+++ Login.java
@@ -0,0 +1,12 @@
+public class Login {
+    public void userName(){
+        System.out.println("enter user name");
+    }
+
+    public void userPassword(){
+        System.out.println("enter password");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
